664c00d0f62b0053fabcda323e235084e6ec2742
Deepak <deepakramchouhan310@gamil.com>
2022-11-02 19:44:37 +0530

    navbar and slider

---
+++ script.js
@@ -0,0 +1,44 @@
+// hamberger menu
+
+let menu =document.querySelector('#menu-bar');
+let navbar =document.querySelector('.navbar');
+
+
+menu.addEventListener('click',() =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+});
+
+
+
+// slider
+var slideIndex = 1;
+        showSlides(slideIndex);
+  
+        function plusSlides(n) {
+          showSlides((slideIndex += n));
+        }
+  
+        function currentSlide(n) {
+          showSlides((slideIndex = n));
+        }
+  
+        function showSlides(n) {
+          var i;
+          var slides = document.getElementsByClassName("mySlides");
+          var dots = document.getElementsByClassName("dot");
+          if (n > slides.length) {
+            slideIndex = 1;
+          }
+          if (n < 1) {
+            slideIndex = slides.length;
+          }
+          for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+          }
+          for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+          }
+          slides[slideIndex - 1].style.display = "block";
+          dots[slideIndex - 1].className += " active";
+        }

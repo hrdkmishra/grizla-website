2091647e09ccc71ae3d2f09ec44cc9e40d90e67d
Viraj Bhagat <virajbhagat03@gmail.com>
2022-11-16 17:53:40 +0530

    Added project & footer section by viraj,arpit,arya :)

--- script.js
+++ script.js
@@ -42,3 +42,39 @@ var slideIndex = 1;
           slides[slideIndex - 1].style.display = "block";
           dots[slideIndex - 1].className += " active";
         }
+
+
+
+// Viraj Js
+// (function ($) {
+
+//   // Portfolio isotope and filter
+//   var portfolioIsotope = $('.portfolio-container').isotope({
+//       itemSelector: '.portfolio-item',
+//       layoutMode: 'fitRows'
+//   });
+//   $('#portfolio-flters li').on('click', function () {
+//       $("#portfolio-flters li").removeClass('active');
+//       $(this).addClass('active');
+
+//       portfolioIsotope.isotope({filter: $(this).data('filter')});
+//   });
+  
+// })(jQuery);
+
+// //  2
+// const scrollbar_hidden=document.querySelector("body");
+// var trailer=document.querySelector(".trailer");
+// function toggle(){
+//   var video=document.querySelector("video");
+//   trailer.classList.toggle("active");
+//   video.pause();
+//   video.currentTime=0;
+//   scrollbar_hidden.classList.add('scrollbar_hide');
+// }
+
+// function toggle_close(){
+//   // const closee=document.querySelector(".close");    
+//   trailer.classList.remove('active');
+//   scrollbar_hidden.classList.remove('scrollbar_hide');
+// }
